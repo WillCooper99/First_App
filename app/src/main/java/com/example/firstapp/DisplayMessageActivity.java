28522bc0fe863d995c7c0630e439897097022f41
Will Cooper <54554479+WillCooper99@users.noreply.github.com>
2021-03-28 09:38:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra(MainActivity.EXTRA_MSG);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(msg);
+    }
+}
